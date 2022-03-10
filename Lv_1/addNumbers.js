7c4ab0590dff2bb2581eee04988ca516bf26722e
Yeongkyeong12 <ohkyeong1995@gmail.com>
2022-03-10 22:26:31 +0900

    Feat: add addNumbers.js

---
+++ Lv_1/addNumbers.js
@@ -0,0 +1,17 @@
+// #5. 두 개 뽑아서 더하기
+
+function solution(numbers) {
+  let answer = [];
+
+  for (let i = 0; i < numbers.length - 1; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      let sum = numbers[i] + numbers[j];
+      if (answer.includes(sum) == false) {
+        answer.push(sum);
+      }
+    }
+  }
+
+  answer.sort((a, b) => a - b);
+  return answer;
+}
